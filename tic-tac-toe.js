9331a5e8b0366e094c311266c8f1b4bae91a643f
Antonio Neil <bossneil20@gmail.com>
2022-10-09 15:04:56 -0500

    Added Placing Mark function with correct color scheme

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,29 +3,63 @@ window.onload = function(){
     console.log(square);
     for (var i = 0; i < square.children.length; i++) {
         square.children[i].classList.add('square');
-    }    
+    }
+    let circleturn = Boolean;
+    circleturn = false;
+    
+    const x_turn = 'X'
+    const O_turn = 'O'
+
+    const Winning_Combinations = [
+    [0,1,2],
+    [0,3,6],
+    [3,4,5],
+    [6,7,8],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+
+    positions = document.querySelectorAll('.square');
+    newgamebutton = document.querySelector('.btn');
+    console.log(newgamebutton);
+    console.log(positions);
+
+    
+
+    newGame();
+
+    newgamebutton.addEventListener('click', newGame);
+
+    function newGame(){
+        circleturn = false;
+        positions.forEach(element => {
+            element.removeEventListener('click', placeMark);
+            element.addEventListener('click', placeMark);
+
+            
+        });
+      
+    }
+
+    function placeMark(element){
+        const pos = element.target;
+        if (circleturn){
+            pos.textContent = "O";
+            pos.classList.add(O_turn);
+            circleturn = false;
+        } else {
+            pos.textContent = "X";
+            pos.classList.add(x_turn);
+            circleturn = true;
+        }
+        
+    }
+
+
+
 };
 
 
-
-//const child = document.getElementById('parent');
-//var child = square.children[i];
-
-//square.children[0].classList.add('square');
-
-//const child = document.getElementById('child');
-
-//child.classList.add('yellow-bg');
-/*
-for (var i = 0; i < square.children.length; i++) {
-    square.children[i].classList.add('square')
-} */
-
-
-/*square.onload = function squares(){
-    for (var i = 0; i < document.getElementById("board").children.length; i++) {
-        var sq = document.getElementById("board").childern[i];
-        console.log(sq.classList.add('square'))
-    }
-} */
 
